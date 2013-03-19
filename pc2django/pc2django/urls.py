ac0990bd8d2be328f443793f215d7b7acb093f41
Karthik Gomadam <karthik.gomadam@gmail.com>
2013-03-19 13:48:02 -0700

    adding urls.py

--- pc2django/pc2django/urls.py
+++ pc2django/pc2django/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
+		url(r'^$','pccrportal.views.home',name='pccr_home'),
     # url(r'^$', 'pc2django.views.home', name='home'),
     # url(r'^pc2django/', include('pc2django.foo.urls')),
 
