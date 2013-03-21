e6a074ec1c218b1113dd6c0da7386f7877bb5324
Karthik Gomadam <karthik.gomadam@gmail.com>
2013-03-21 12:36:33 -0700

    adding new css files, images, and updated landing

--- pc2django/pc2django/pccrportal/views.py
+++ pc2django/pc2django/pccrportal/views.py
@@ -3,4 +3,12 @@
 from django.shortcuts import render_to_response
 
 def home(request):
-	return render_to_response('index.html')
+	return render_to_response('pccrportal/index.html')
+
+def register_researcher(request):
+	return render_to_response('researcher/register.html')
+
+def register_participant(request):
+	return render_to_response('participant/register.html')
+
+
