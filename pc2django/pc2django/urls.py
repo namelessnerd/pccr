b1776fd0ecc1572c423158d598af5f7ebc7a76d3
Sophia Cao <sophia.c.duke@gmail.com>
2013-03-21 16:42:35 -0700

    Syncing urls.py

--- pc2django/pc2django/urls.py
+++ pc2django/pc2django/urls.py
@@ -7,13 +7,12 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     # Examples:
 		url(r'^$','pccrportal.views.home',name='pccr_home'),
-		url(r'^researcher/register$','pccrportal.views.register_researcher',name='pccr_home'),
+		url(r'^researcher/participant/register$','pccrportal.views.register_researcher',name='pccr_home'),
     # url(r'^$', 'pc2django.views.home', name='home'),
+    url(r'^register/', 'pccrportal.views.register_researcher',name='register_researcher'),
+		# url(r'^$', 'pc2django.views.home', name='home'),
     # url(r'^pc2django/', include('pc2django.foo.urls')),
-
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
