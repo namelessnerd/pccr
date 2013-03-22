2c21736096c9ad488be279267674fa67393d9f8f
Sophia Cao <sophia.c.duke@gmail.com>
2013-03-21 22:38:57 -0700

    Clean ups and UI Additions
    CLEAN UPS
    -Fixed indentation (from Sublime Text)
    -Moved footer to center and changed content
    -Removed register_participant from urls.py
    -Removed register_participant from views.py
    -Commented out old urls.py lines and moved to bottom
    
    UI ADDITIONS
    -Added icons to hero unit on landing page
    -Fixed Researcher Registration page

--- pc2django/pc2django/pccrportal/views.py
+++ pc2django/pc2django/pccrportal/views.py
@@ -3,13 +3,14 @@
 from django.shortcuts import render_to_response
 
 def home(request):
-	return render_to_response('pccrportal/index.html',{'title':'Platform for Patient Centric Collaborative Care'})
+	return render_to_response('pccrportal/index.html',{'title':'Platform for Patient Centric Collaborative Care','link_class_home':'active',})
 
 def register_researcher(request):
-	return render_to_response('pccrportal/register.html')
+	return render_to_response('researcher/register.html')
+
+def about(request):
+	return render_to_response('about.html',{'title':'About PCCR','link_class_about':'active',})
+
+#def register_participant(request):
+	#return render_to_response('pccrportal/register.html')
 	#return render_to_response('researcher/register.html')
-
-def register_participant(request):
-	return render_to_response('participant/register.html')
-
-
