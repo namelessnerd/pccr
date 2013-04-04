eda89c45115342302005742c8211c5409c08e576
Karthik Gomadam <karthik.gomadam@gmail.com>
2013-04-03 17:24:02 -0700

    adding urls to reflect changes on register and explore

--- me_med/me_med/urls.py
+++ me_med/me_med/urls.py
@@ -6,9 +6,9 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
 	url(r'^memed/$','appy.views.home',name='pccr_home'),
-	url(r'^memed/register/$','appy.views.register',name='pccr_home'),
-	url(r'^memed/registerme/$','appy.views.registerme',name='pccr_home'),
-	url(r'^memed/explore/$','appy.views.forum_explore',name='forum_explore'),
+	url(r'^memed/login/$','appy.views.login_user',name='memed_user_login'),
+	url(r'^memed/logout/$','appy.views.logout_user',name='memed_user_logout'),
+	url(r'^memed/registerme/$','appy.views.registerme',name='memed_user_register'),
     # Examples:
     # url(r'^$', 'proj.views.home', name='home'),
     # url(r'^proj/', include('proj.foo.urls')),
