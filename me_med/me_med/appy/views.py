adcec0ce40000b2fc8286d14d9c9c7bccd41b2ef
Karthik Gomadam <karthik.gomadam@gmail.com>
2013-04-04 17:24:48 -0700

    created scaffolding for the patient pages. will need to populate.

--- me_med/me_med/appy/views.py
+++ me_med/me_med/appy/views.py
@@ -1,8 +1,10 @@
 # Create your views here.
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 from django.core.context_processors import csrf
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
 
 def home(request):
 	c = {'title':'Welcome to Me-Med'}
@@ -19,7 +21,7 @@ def login_user(request):
 		if user is not None:
 				if user.is_active:
 						login(request, user)
-						return render_to_response('forum_register_confirm.html',{'user_signed':True,'userid':username, })
+						return redirect('/memed/user')
 				else:
 						c = {'title':'Welcome to Me-Med','login_error':True, 'message':'Your account is no longer active. Register again.', }
 						c.update(csrf(request))
@@ -44,7 +46,12 @@ def registerme(request):
 	user.save()
 	user= authenticate(username= request.POST['email'],password= request.POST['password'])
 	login(request, user)
-	return render_to_response('forum_register_confirm.html',{'user_signed':True,'userid':request.POST['email'],})
+	return redirect('/memed/user')
+
+@login_required
+def show_profile(request):
+	return render_to_response('user_profile.html',{'user_signed':True,'userid':request.user.username,})
+
 
 def forum_explore(request):
 		return render_to_response('forum_view.html')
--- me_med/me_med/urls.py
+++ me_med/me_med/urls.py
@@ -9,6 +9,7 @@ urlpatterns = patterns('',
 	url(r'^memed/login/$','appy.views.login_user',name='memed_user_login'),
 	url(r'^memed/logout/$','appy.views.logout_user',name='memed_user_logout'),
 	url(r'^memed/registerme/$','appy.views.registerme',name='memed_user_register'),
+	url(r'^memed/user/$','appy.views.show_profile',name='memed_user_register'),
 	url(r'^memed/explore/$','appy.views.forum_explore',name='forum_explore'),
     # Examples:
     # url(r'^$', 'proj.views.home', name='home'),
